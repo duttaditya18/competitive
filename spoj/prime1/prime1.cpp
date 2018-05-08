cd6086ce389f681fcf7c276017cf9389b43bfb6c
Aditya Dutt <duttaditya18@gmail.com>
2018-05-08 13:25:41 +0530

    ?

--- spoj/prime1/prime1.cpp
+++ spoj/prime1/prime1.cpp
@@ -1,43 +1,9 @@
+// Wrong Answer?
+
 #include <iostream>
  
 using namespace std;
- 
-int checkprime(int num);
-int genprime(int n1, int n2);
- 
-int main(void)
-{
-	int test;
-	cin >> test;
-	for(int i = 0; i < test; i++)
-	{
-		int n1;
-		cin >> n1;
-		int n2;
-		cin >> n2;
-		genprime(n1, n2);
-	}
-}
- 
-int genprime(int n1, int n2)
-{
-	for(int i = n1; i <= n2; i++)
-	{
-		int checkc = checkprime(i);
-	    if(checkc == 0)
-	    {
-			continue;
-		}
-		else
-		{
-			cout << i;
-			cout << "\n";
-		}
-	}
-	cout << "\n";
-}
- 
- 
+
 int checkprime(int num)
 {
 	for(int f = 2; f <= num / 2; f++)
@@ -53,3 +19,38 @@ int checkprime(int num)
 		return 1;
 	}
 }
+
+int genprime(int n1, int n2)
+{
+	for(int i = n1; i <= n2; i++)
+	{
+		int checkc = checkprime(i);
+	    if(checkc == 0)
+	    {
+			continue;
+		}
+		else
+		{
+			cout << i << endl;
+		}
+	}
+	cout << "\n";
+}
+ 
+ 
+int main(void)
+{
+	int test;
+	cin >> test;
+	for(int i = 0; i < test; i++)
+	{
+		int n1;
+		cin >> n1;
+		int n2;
+		cin >> n2;
+		genprime(n1, n2);
+	}
+	
+	return 0;
+}
+
