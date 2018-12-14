f4ce687f9bf2c8ed3e304924623d65fbe11e949d
Aditya Dutt <duttaditya18@gmail.com>
2018-12-14 14:42:08 +0530

    Fixed 118A

--- codeforces/118A/118A.cpp
+++ codeforces/118A/118A.cpp
@@ -10,13 +10,15 @@ int main()
   cin >> str;
 
   string newStr;
-
+  int c = 0;
   for (int i = 0; i < str.size(); i++)
   {
+    cout << i << " : " << str[i] << " : " << newStr << endl;
     if (!(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'y' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' || str[i] == 'Y'))
     {
-      newStr.insert((i), ".");
+      newStr.insert((c), ".");
       newStr.push_back(str[i]);
+      c += 2;
     }
   }
   cout << newStr;
