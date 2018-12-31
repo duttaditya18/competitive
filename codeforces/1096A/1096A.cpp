6a2c634073a44e219f86296c974bc90b16465efc
Aditya Dutt <duttaditya18@gmail.com>
2019-01-01 01:10:00 +0530

    lol ez : 1096A

---
+++ codeforces/1096A/1096A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  int t;
+  cin >> t;
+
+  for (int i = 0; i < t; i++)
+  {
+    int l, r;
+    cin >> l >> r;
+
+    cout << l << " " << l * 2 << endl;
+  }
+
+  return 0;
+}
