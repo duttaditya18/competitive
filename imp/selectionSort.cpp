42e2e00e6b6889e360ecfa0254ed26bb6bab5ce0
Aditya Dutt <duttaditya18@gmail.com>
2019-01-12 10:56:32 +0530

    Added sS

---
+++ imp/selectionSort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int selectionSort(int a[], int n)
+{
+  int minI, i, j;
+  for (i = 0; i < n; i++)
+  {
+    minI = i;
+    for (j = i; j < n; j++)
+      if (a[j] < a[minI])
+        minI = j;
+    swap(a[i], a[minI]);
+  }
+}
+
+int main(void)
+{
+  int a[8] = {9, 5, 4, 6, 1, 10, 7, 4};
+  selectionSort(a, sizeof(a) / sizeof(a[0]));
+  for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+    printf("%d ", a[i]);
+  return 0;
+}
