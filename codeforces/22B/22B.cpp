feef9125928df1851a6a30c66d5381d1448b6ccb
Aditya Dutt <duttaditya18@gmail.com>
2019-01-24 00:39:56 +0530

    did it ! : CF 22B

--- codeforces/22B/22B.cpp
+++ codeforces/22B/22B.cpp
@@ -1,9 +1,12 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-int n, m, far[25][25];
-char grid[25][25];
+int n, m, out = 0;
+// this somehow initialises everything to 0
+int dp[27][27] = {};
+char grid[27][27];
 
 int main(void)
 {
@@ -12,7 +15,31 @@ int main(void)
 
   cin >> n >> m;
 
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < m; j++)
-      cin >> grid[n][m];
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= m; j++)
+    {
+      cin >> grid[i][j];
+      // calc how many 1's are there on left and up and includes iteself. think about sets sorta
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + (grid[i][j] == '1');
+    }
+  }
+
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= m; j++)
+    {
+      for (int k = i; k <= n; k++)
+      {
+        for (int l = j; l <= m; l++)
+        {
+          // see supersonic nb last pages
+          // basically it calculates if there are any zeroes in a given area. think about sets
+          if (dp[i - 1][l] + dp[k][j - 1] - dp[i - 1][j - 1] == dp[k][l])
+            out = max(out, 2 * ((k - i + 1) + (l - j + 1)));
+        }
+      }
+    }
+  }
+  cout << out;
 }
