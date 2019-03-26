d778f00fbe44c00dfa49a929b8c74415e0eac4c3
Aditya Dutt <duttaditya18@gmail.com>
2019-03-26 13:04:50 +0530

    usaco:train:friday

---
+++ usaco/train/friday.cpp
@@ -0,0 +1,15 @@
+/*
+ID: duttadi1
+TASK: fridaay
+LANG: C++                 
+*/
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  ifstream cin("friday.in");
+  ofstream cout("friday.out");
+}
