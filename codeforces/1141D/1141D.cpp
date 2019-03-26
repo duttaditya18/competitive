0bb2e86656a4e06360b2f9773a68c479980e108a
Aditya Dutt <duttaditya18@gmail.com>
2019-03-26 09:55:22 +0530

    CF:1141D (TLE)

---
+++ codeforces/1141D/1141D.cpp
@@ -0,0 +1,110 @@
+// TLE
+
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+  int n;
+  vector<pair<int, int>> vect;
+  cin >> n;
+
+  string l, r;
+  cin >> l >> r;
+
+  int lCount[n] = {0}, rCount[n] = {0};
+
+  // l-defined to r-defined
+  for (int i = 0; i < n; i++)
+  {
+    if (l[i] != '?')
+    {
+      for (int j = 0; j < n; j++)
+      {
+        if (lCount[i] == 0 && rCount[j] == 0)
+        {
+          if (l[i] == r[j])
+          {
+            lCount[i]++;
+            rCount[j]++;
+            vect.push_back(make_pair(i, j));
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  // l-undefined to r-defined
+  for (int i = 0; i < n; i++)
+  {
+    if (l[i] == '?')
+    {
+      for (int j = 0; j < n; j++)
+      {
+        if (lCount[i] == 0 && rCount[j] == 0)
+        {
+          if (r[j] != '?')
+          {
+            lCount[i]++;
+            rCount[j]++;
+            vect.push_back(make_pair(i, j));
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  // r-undefined to l-defined
+  for (int i = 0; i < n; i++)
+  {
+    if (r[i] == '?')
+    {
+      for (int j = 0; j < n; j++)
+      {
+        if (rCount[i] == 0 && lCount[j] == 0)
+        {
+          if (l[j] != '?')
+          {
+            rCount[i]++;
+            lCount[j]++;
+            vect.push_back(make_pair(j, i));
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  // l-undefined to r-undefined
+  for (int i = 0; i < n; i++)
+  {
+    if (l[i] == '?')
+    {
+      for (int j = 0; j < n; j++)
+      {
+        if (lCount[i] == 0 && rCount[j] == 0)
+        {
+          if (l[i] == '?' && r[j] == '?')
+          {
+            lCount[i]++;
+            rCount[j]++;
+            vect.push_back(make_pair(i, j));
+            break;
+          }
+        }
+      }
+    }
+  }
+  cout << vect.size() << endl;
+  for (int i = 0; i < vect.size(); i++)
+  {
+    cout << vect[i].first + 1 << " "
+         << vect[i].second + 1 << endl;
+  }
+}
