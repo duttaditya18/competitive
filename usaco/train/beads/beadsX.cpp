7b1578fe71e5cb18ad7180419e424061be8e8623
Aditya Dutt <duttaditya18@gmail.com>
2019-03-28 12:29:32 +0530

    Added DP beadsX

---
+++ usaco/train/beads/beadsX.cpp
@@ -0,0 +1,21 @@
+/*
+ID: duttadi1
+TASK: beads
+LANG: C++                 
+*/
+
+// DP : O(n)
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+  ifstream cin("beads.in");
+  ofstream cout("beads.out");
+
+  return 0;
+}
