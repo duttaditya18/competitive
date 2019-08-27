21a99bdeaa34c17a4028711503bd385d1082b2a8
Aditya Dutt <duttaditya18@gmail.com>
2019-08-27 07:21:44 +0530

    RBO

--- codeforces/1208B/1208B.cpp
+++ codeforces/1208B/1208B.cpp
@@ -10,12 +10,41 @@ int main(void)
     {
         cin >> a[i];
     }
-    int ans = n - 1;
 
     map <int, int> mp;
-
-    for(int i = 0; i < n; i++)
+    bool f = true;
+    int ans = n - 1, x;
+    for(int i = 0; (i < n) && (f); i++)
     {
-
+        //cout << "i: " << i << endl;
+        x = 0;
+        for(int j = 0; j <= i; j++)
+        {
+            //cout << "j: " << j << endl;
+            mp[a[j]]++;
+            if(mp[a[j]] > 1)
+            {
+                f = false;
+                break;
+            }
+            x++;
+        }
+        if(f)
+        {
+            for(int j = n - 1; j > i; j--)
+            {
+                //cout << "j: " << j << endl;
+                mp[a[j]]++;
+                if(mp[a[j]] > 1)
+                {
+                    break;
+                }
+                x++;
+            }
+            ans = min(ans, n - x);
+        }
+        //cout << endl;
+        mp.clear();
     }
+    cout << ans << endl;
 }
