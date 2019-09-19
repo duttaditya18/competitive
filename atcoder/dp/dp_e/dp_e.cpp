78edfbd9fbaa2fe5355bc7de7696eb611e6e4c97
Aditya Dutt <duttaditya18@gmail.com>
2019-09-19 10:13:42 +0530

    AtCoder DP E WA WTF WHY

--- atcoder/dp/dp_e/dp_e.cpp
+++ atcoder/dp/dp_e/dp_e.cpp
@@ -1,30 +1,56 @@
-// TLE obviously
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(void)
 {
-    int n, x;
-    cin >> n >> x;
+    int n, W;
+    cin >> n >> W;
 
     int w[n], v[n];
-
-    for(int i = 0; i < n; i++) cin >> w[i] >> v[i];
-
-    long long a[x + 1] = {};
-
+    int maxv = 0;
     for(int i = 0; i < n; i++)
     {
-        for(int j = x; j >= 1; j--)
+        cin >> w[i] >> v[i];
+        maxv += v[i];
+    }
+
+    int dp[n][maxv + 1] = {};
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j <= maxv; j++)
+            dp[i][j] = 2000000000;
+
+
+    dp[0][v[0]] = w[0];
+    for(int i = 1; i < n; i++)
+    {
+        dp[i][v[i]] = min(dp[i][v[i]], w[i]);
+        for(int j = 0; j <= maxv; j++)
         {
-            if(j >= w[i])
+            dp[i][j] = min(dp[i - 1][j], dp[i][j]);
+            if(j >= v[i])
             {
-                a[j] = max(a[j], a[j - w[i]] + v[i]);
+                dp[i][j] = min(dp[i - 1][j - v[i]] + w[i], dp[i][j]);
             }
         }
     }
-    cout << a[x] << endl;
+
+    int ans = 0;
+
+    for(int i = 0; i <= maxv; i++)
+    {
+        if(dp[n - 1][i] <= W)
+        {
+            ans = max(ans, i);
+        }
+    }
+
+    cout << ans << endl;
+
+    /*for(int i = 0; i < n; i++)
+        for(int j = 0; j <= maxv; j++)
+            cout << i << " : " << j << " : " << dp[i][j] << endl;*/
+
     return 0;
 }
