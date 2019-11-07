3ca9de363f51d5c490288c71dedd1e824e30255a
Aditya Dutt <duttaditya18@gmail.com>
2019-11-08 03:06:17 +0530

    HR: handshake

---
+++ hackerrank/handshake/handshake.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << (1ll * n)*(n - 1)/2 << endl;
+    }
+    return 0;
+}
